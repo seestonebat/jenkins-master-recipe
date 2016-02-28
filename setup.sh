0856dcc30dd5719d0d34df94221b74487a236e6c
Ubuntu <ubuntu@ip-172-31-24-58.us-west-2.compute.internal>
2016-02-28 22:56:16 +0000

    Initial checkin of setup file.

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script installs jenkins on bare instances
+
+# Exiti if jenkins laready present
+if dpkg-query -l jenkins > /dev/null ; then 
+    echo "ERROR: Jenkins is already installed"
+    exit 1
+fi
+
+# Install jenkins package
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install --assume-yes jenkins
+
+# Forward port 8080 to 80
+sudo perl -0777 -i.original -pe 's/exit 0/#Requests from outside\niptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+\n#Requests from localhost\niptables -t nat -I OUTPUT -p tcp -d 127.0.0.1 --dport 80 -j REDIRECT --to-ports 8080\nexit 0/igs' /etc/rc.local
+sudo /etc/rc.local
+
+sudo service jenkins start
+
+echo "************ SUCCESSFULLY INSTALLED JENKINS *********************"
+exit 0
